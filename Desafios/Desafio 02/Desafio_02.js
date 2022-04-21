f677142e3226173e0e1ea1987294160945f35471
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2022-04-21 09:53:36 -0300

    Desafio 02 Mejorado

--- Desafios/Desafio 02/Desafio_02.js
+++ Desafios/Desafio 02/Desafio_02.js
@@ -1,93 +1,90 @@
 console.log("Desafio 02");
 const fs = require('fs');
-const string_nombreArchivo = "./Desafios/Desafio 02/productos.txt";
+const string_nombreArchivo = "./productos.txt";
 
 class Contenedor {
-    constructor(productos) {
-        this.objectArray = productos;
+    constructor(nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
     }
-    save(producto) {
-        if(this.objectArray.length == 0) {
+    async save(producto) {
+        let contenido = await this.getAll();
+        if(contenido.length == 0) {
             producto.id = 1;
         } else {
-            producto.id = this.objectArray[this.objectArray.length-1].id + 1;
-        }        
-        this.objectArray.push(producto);
+            producto.id = contenido[contenido.length-1].id + 1;
+        }
+        await contenido.push(producto);
         try{
-            fs.writeFileSync(string_nombreArchivo, JSON.stringify(this.objectArray, null, 2));
+            await fs.promises.writeFile(this.nombreArchivo, JSON.stringify(contenido, null, 2));
+            return contenido.length;
         }
         catch (error) {
             throw new Error(`Error en escritura: ${error.message}`);
         }
-        return this.objectArray.length;
     }
-    getById(id) {
-        let object_encontrado = this.objectArray.find(producto => producto.id == id);
+    async getById(id) {
+        let contenido = await this.getAll();
+        let object_encontrado = contenido.find(producto => producto.id == id);
         if(object_encontrado == undefined) {
             return null;
         } else {
             return object_encontrado;
         }
     }
-    getAll() {
-        return this.objectArray;
-    }
-    deleteById(id) {
-        this.objectArray = this.objectArray.filter(producto => producto.id != id);
-        try{
-            fs.writeFileSync(string_nombreArchivo, JSON.stringify(this.objectArray, null, 2));
-        }
-        catch (error) {
-            throw new Error(`Error en escritura: ${error.message}`);
-        }
-    }
-    deleteAll() {
-        this.objectArray = [];
-        try{
-            fs.writeFileSync(string_nombreArchivo, JSON.stringify([], null, 2));
-        }
-        catch (error) {
-            throw new Error(`Error en escritura: ${error.message}`);
-        }
-    }
-}
-
-function leerProductos() {
-    try {
-        let contenido = fs.readFileSync(string_nombreArchivo, 'utf-8');
-        if(contenido.length == 0) {
-            return [];//en el caso de que el archivo esté completamente vacío
-        }
-        else {
+    async getAll() {
+        try {
+            let contenido = await fs.promises.readFile(this.nombreArchivo, 'utf-8');
             return JSON.parse(contenido);
         }
+        catch (error) {
+            return [];
+        }
     }
-    catch (error) {
-        throw new Error(`Error en lectura: ${error.message}`);
+    async deleteById(id) {
+        let contenido = await this.getAll();
+        contenido = contenido.filter(producto => producto.id != id);
+        try{
+            await fs.promises.writeFile(this.nombreArchivo, JSON.stringify(contenido, null, 2));
+        }
+        catch (error) {
+            throw new Error(`Error en escritura: ${error.message}`);
+        }
+    }
+    async deleteAll() {
+        try{
+            await fs.promises.writeFile(this.nombreArchivo, JSON.stringify([], null, 2));
+        }
+        catch (error) {
+            throw new Error(`Error en escritura: ${error.message}`);
+        }
     }
 }
 
-const contenedor_productos = new Contenedor(leerProductos());
 
-let producto;
-let int_indiceAgregado;
-for (let i = 0; i < 10; i++) {
-    producto = {
-        title: 'Escuadra', 
-        price: Math.floor(Math.random()*100),
-        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'
-    };
-    int_indiceAgregado = contenedor_productos.save(producto);
-    console.log(int_indiceAgregado);
-}
+(async function(){
+    const contenedor_productos = new Contenedor(string_nombreArchivo);
+    let producto;
+    let int_indiceAgregado;
 
-console.log(contenedor_productos.getById(6));
-console.log(contenedor_productos.getById(23));
+    for (let i = 0; i < 10; i++) {
+        producto = {
+            title: 'Escuadra', 
+            price: Math.floor(Math.random()*100),
+            thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'
+        };
+        int_indiceAgregado = await contenedor_productos.save(producto);
+        console.log("Indice Agregado: " + int_indiceAgregado, producto);
+    }
 
-console.log(contenedor_productos.getAll());
+    console.log(await contenedor_productos.getById(6));
 
-contenedor_productos.deleteById(2);
+    console.log(await contenedor_productos.getById(23));
 
-console.log(contenedor_productos.getAll());
+    console.log(await contenedor_productos.getAll());
 
-contenedor_productos.deleteAll();
+    await contenedor_productos.deleteById(2);
+
+    console.log(await contenedor_productos.getAll());
+
+    await contenedor_productos.deleteAll();
+})();
