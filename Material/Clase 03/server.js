6a0424690a013a45a6943ed7b334c755d96f1a42
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2022-04-23 15:51:08 -0300

    Clase 03

--- Material/Clase 03/server.js
+++ Material/Clase 03/server.js
@@ -4,6 +4,11 @@ const app = express();
 
 const PORT = 8080;
 
+app.get('/', (req, res) => {
+    //res.end("<h1>Hola Mundo</h1>");
+    res.send("<h1>Hola Mundo</h1>");
+})
+
 const server = app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${server.address().port}`);
 });
