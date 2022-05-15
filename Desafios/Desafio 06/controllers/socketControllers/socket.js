f5c594f2a0bb4f2b9d83f5f5041e77aa24c81287
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2022-05-15 13:55:46 -0300

    Desafio 06

--- Desafios/Desafio 06/controllers/socketControllers/socket.js
+++ Desafios/Desafio 06/controllers/socketControllers/socket.js
@@ -1,17 +1,21 @@
 const {Contenedor} = require('./../../classes/Contenedor.js');
-const rutaProductos = "./productos.txt"
-const productos = new Contenedor(rutaProductos);
+const productos = new Contenedor("./data/productos.txt");
+const mensajes = new Contenedor('./data/mensajes.txt');
 
 async function socketController(socket, io) {
     console.log('Usuario conectado: ' + socket.id)
     const array_productos = await productos.getAll();
-    socket.emit('connectionToServer', { array_productos })
+    const array_mensajes = await mensajes.getAll();
+    socket.emit('connectionToServer', { array_productos, array_mensajes })
 
     socket.on('agregarProducto', async (data) => {
-        const { title, price, thumbnail } = data;
-        const producto = { title, price, thumbnail }
-        await productos.save(producto);
+        await productos.save(data);
         io.sockets.emit('actualizarTabla', { array_productos: await productos.getAll() })
+    })
+
+    socket.on("enviarMensaje", async (data) => {
+        await mensajes.save(data);
+        io.sockets.emit('actualizarMensajes', { array_mensajes: await mensajes.getAll() })
     })
 }
 
--- Desafios/Desafio 06/public/js/index.js
+++ Desafios/Desafio 06/public/js/index.js
@@ -1,14 +1,29 @@
 const socket = io()
 
-socket.on('connectionToServer', async ({ array_productos }) => {
-    mostrarProductos(array_productos);
-    await mostrarFormulario();
+socket.on('connectionToServer', async ({ array_productos, array_mensajes }) => {
+    await mostrar('formProducts', 'templates/form.handlebars', {});
+    actualizarProductos(array_productos);
+    await actualizarMensajes(array_mensajes);
     agregarFuncionABotones();
 });
 
 socket.on('actualizarTabla', ({ array_productos }) => {
-    mostrarProductos(array_productos);
+    actualizarProductos(array_productos);
 });
+
+socket.on('actualizarMensajes', ({ array_mensajes }) => {
+    actualizarMensajes(array_mensajes);
+})
+
+const actualizarProductos = async (array_productos) => {
+    let context = { titulo:"Productos", array_productos, hayProductos: array_productos.length > 0, total: array_productos.length };
+    mostrar('tableProducts', 'templates/table.handlebars', context);
+}
+
+const actualizarMensajes = async (array_mensajes) => {
+    context = { array_mensajes, hayMensajes: array_mensajes.length > 0 }
+    await mostrar('mensajes', 'templates/messages.handlebars', context);
+}
 
 function agregarFuncionABotones() {
     const btn = document.getElementById('botonEnviar')
@@ -16,18 +31,29 @@ function agregarFuncionABotones() {
         const title = document.getElementById('title').value
         const price = document.getElementById('price').value
         const thumbnail = document.getElementById('thumbnail').value
-        socket.emit('agregarProducto', { title, price, thumbnail })
+        if(title.length>0 && price.length>0 && thumbnail.length>0){
+            socket.emit('agregarProducto', { title, price, thumbnail })
+        } else {
+            alert('Todos los campos son obligatorios')
+        }
+    })
+    const btn2 = document.getElementById("botonEnviarMensaje")
+    btn2.addEventListener('click', event => {
+        const email = document.getElementById('email').value
+        const mensaje = document.getElementById('mensaje').value
+        const fecha = new Date();
+        const fechaString = fecha.getFullYear() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getDate() + " " + fecha.getHours() + ":" + fecha.getMinutes() + ":" + fecha.getSeconds();
+        if(email.length>0 && mensaje.length>0){
+            socket.emit('enviarMensaje', { email, mensaje, fechaString })
+        } else {
+            alert('Todos los campos son obligatorios')
+        }
     })
 }
 
-async function mostrarProductos(array_productos) {
-    const divProductos = document.getElementById('tableProducts');
-    divProductos.innerHTML = await armarTabla(array_productos)
-}
-
-async function armarTabla(array_productos) {
-    const contexto = { titulo:"Productos", array_productos, hayProductos: array_productos.length > 0, total: array_productos.length }
-    return armarHtmlRemoto('templates/table.handlebars', contexto)
+async function mostrar(id, template, context) {
+    const divProductos = document.getElementById(id);
+    divProductos.innerHTML = await armarHtmlRemoto(template, context);
 }
 
 function armarHtmlRemoto(url, contexto) {
@@ -39,13 +65,4 @@ function armarHtmlRemoto(url, contexto) {
 
 function buscarPlantilla(url) {
     return fetch(url).then(res => res.text())
-}
-
-async function mostrarFormulario() {
-    const divProductos = document.getElementById('formProducts');
-    divProductos.innerHTML = await armarFormulario()
-}
-
-async function armarFormulario() {
-    return armarHtmlRemoto('templates/form.handlebars', {})
 }
