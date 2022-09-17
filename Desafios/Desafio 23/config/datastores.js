68355389d90c28486411579034a55e51afd562b4
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2022-09-17 14:43:58 -0300

    Desafio 23

---
+++ Desafios/Desafio 23/config/datastores.js
@@ -0,0 +1,10 @@
+module.exports.datastores = {
+
+
+  default: {
+    adapter: 'sails-mongo',
+    url: 'mongodb+srv://fran2109:Fran-2109@cluster0.9ghdw.mongodb.net/ecommerce?retryWrites=true&w=majority'
+  },
+
+
+};
